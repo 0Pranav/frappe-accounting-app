a6f67b32cf832cb2d2aefdb9c4560f13c1b2f55b
pranav nachnekar <pranav@pranavs-MacBook-Pro.local>
2019-08-07 17:00:27 +0530

    General ledger entry after sales invoice creation

--- accounting/accounting/doctype/sales_invoice/sales_invoice.py
+++ accounting/accounting/doctype/sales_invoice/sales_invoice.py
@@ -3,8 +3,42 @@
 # For license information, please see license.txt
 
 from __future__ import unicode_literals
-# import frappe
+import frappe
+import datetime
 from frappe.model.document import Document
 
 class SalesInvoice(Document):
-	pass
+	def save(self, *args, **kwargs):
+		customer = frappe.get_doc("Customer",self.customer)
+		customer_account = frappe.get_doc("Account",customer.customer_account)
+		sales_account = frappe.get_doc("Account","Sales Income")
+		
+		super().save(self,*args,**kwargs)
+		
+		# Adjust balance in accounts
+		customer_account.balance = customer_account.balance - self.total
+		customer_account.save()
+		sales_account.balance = sales_account.balance + self.total
+		sales_account.save()
+		
+		# Debit Entry
+		debit_entry = frappe.new_doc("General Ledger")
+		debit_entry.posting_date = datetime.date.today()
+		debit_entry.account_affected = sales_account.name
+		debit_entry.credited_amount = 0
+		debit_entry.debited_amount = self.total
+		debit_entry.account_balance = sales_account.account_balance
+		debit_entry.txn_type = "Sales Invoice"
+		debit_entry.txn_id = self.name
+		debit_entry.insert()
+
+		# Credit Entry
+		credit_entry = frappe.new_doc("General Ledger")
+		debit_entry.posting_date = datetime.date.today()
+		debit_entry.account_affected = customer_account.name
+		debit_entry.credited_amount = self.total
+		debit_entry.debited_amount = 0
+		debit_entry.account_balance = customer_account.balance
+		debit_entry.txn_type = "Sales Invoice"
+		debit_entry.txn_id = self.name
+		debit_entry.insert()
