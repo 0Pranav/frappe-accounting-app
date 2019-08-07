57efb9ec3a9031c5a93c55ee81b12e9110ffeab0
pranav nachnekar <pranav@pranavs-MacBook-Pro.local>
2019-08-07 16:16:26 +0530

    Removed old general ledger

--- accounting/accounting/doctype/general_ledger/general_ledger/genral_ledger.js
+++
@@ -1,8 +0,0 @@
-// Copyright (c) 2019, 0Pranav and contributors
-// For license information, please see license.txt
-
-frappe.ui.form.on('Genral Ledger', {
-	// refresh: function(frm) {
-
-	// }
-});
