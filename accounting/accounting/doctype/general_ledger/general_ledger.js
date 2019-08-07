8d568a42ce8fac02930e28f32e5e9cbe6002533e
pranav nachnekar <pranav@pranavs-MacBook-Pro.local>
2019-08-07 16:16:02 +0530

    Added general ledger

--- accounting/accounting/doctype/general_ledger/general_ledger.js
+++ accounting/accounting/doctype/general_ledger/general_ledger.js
@@ -1,7 +1,7 @@
 // Copyright (c) 2019, 0Pranav and contributors
 // For license information, please see license.txt
 
-frappe.ui.form.on('General Ledger', {
+frappe.ui.form.on('General Ledgar', {
 	// refresh: function(frm) {
 
 	// }
