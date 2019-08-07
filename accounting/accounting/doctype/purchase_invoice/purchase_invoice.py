0daea0aa21b00540ca302df50d4b55cbf12166e4
pranav nachnekar <pranav@pranavs-MacBook-Pro.local>
2019-08-07 16:17:51 +0530

    Generate general ledger entry on purchase invoice

--- accounting/accounting/doctype/purchase_invoice/purchase_invoice.py
+++ accounting/accounting/doctype/purchase_invoice/purchase_invoice.py
@@ -3,11 +3,42 @@
 # For license information, please see license.txt
 
 from __future__ import unicode_literals
-# import frappe
+import datetime
+import frappe
 from frappe.model.document import Document
 
+
 class PurchaseInvoice(Document):
-	def save(self,*args,**kwargs):
-		# Create general ledger entry here
-		ledger_entry = frappe.new_doc('General Ledger')
-		super.save(self,*args,**kwargs)
+    def save(self, *args, **kwargs):
+        # Create general ledger entry here
+        supplier = frappe.get_doc("Supplier", self.supplier)
+        supplier_account = frappe.get_doc("Account", supplier.supplier_account)
+        stock_account = frappe.get_doc("Account", "Stock Expense")
+
+        super().save(self, *args, **kwargs)
+        supplier_account.account_balance = supplier_account.account_balance - self.total
+        stock_account.account_balance = stock_account.account_balance + self.total
+        debit_entry = frappe.get_doc({
+            "doctype": "General Ledger",
+            "posting_date": datetime.date.today(),
+            "account_affected": stock_account.name,
+            "credited_amount": 0,
+            "debited_amount": self.total,
+            "account_balance": stock_account.account_balance,
+            "txn_type": "Purchase Invoice",
+            "txn_id": self.name,
+        })
+        credit_entry = frappe.get_doc({
+            "doctype": "General Ledger",
+            "posting_date": datetime.date.today(),
+            "account_affected": supplier_account.name,
+            "account_balance": supplier_account.account_balance,
+            "credited_amount": self.total,
+            "debited_amount": 0,
+            "txn_type": "Purchase Invoice",
+            "txn_id": self.name,
+        })
+        debit_entry.insert()
+        credit_entry.insert()
+        supplier_account.save()
+        stock_account.save()
