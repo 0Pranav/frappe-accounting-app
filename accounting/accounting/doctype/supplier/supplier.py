071c0ffb024f8362a0765e6ac4eb36a40c64793b
pranav nachnekar <pranav@pranavs-MacBook-Pro.local>
2019-08-07 16:17:19 +0530

    Added account to supplier

--- accounting/accounting/doctype/supplier/supplier.py
+++ accounting/accounting/doctype/supplier/supplier.py
@@ -3,8 +3,17 @@
 # For license information, please see license.txt
 
 from __future__ import unicode_literals
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 class Supplier(Document):
-	pass
+	def save(self,*args,**kwargs):
+		supplier_payment = frappe.get_doc("Account","Supplier Payment")
+		supplier_account = frappe.new_doc("Account")
+		supplier_account.account_name = self.supplier_name
+		supplier_account.account_type = "Liability"
+		supplier_account.account_balance = 0
+		supplier_account.parent_account = supplier_payment.name
+		supplier_account.insert()
+		self.supplier_account = supplier_account.name
+		super().save(*args,**kwargs)
