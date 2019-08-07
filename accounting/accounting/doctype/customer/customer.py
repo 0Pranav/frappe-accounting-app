fb8b29df967bc74d9a79c93f44f619cda8a67d3d
pranav nachnekar <pranav@pranavs-MacBook-Pro.local>
2019-08-07 16:15:37 +0530

    Add accounts to a customer

--- accounting/accounting/doctype/customer/customer.py
+++ accounting/accounting/doctype/customer/customer.py
@@ -3,8 +3,17 @@
 # For license information, please see license.txt
 
 from __future__ import unicode_literals
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 class Customer(Document):
-	pass
+	def save(self,*args,**kwargs):
+		customer_payments = frappe.get_doc("Account","Customer Payments")
+		customer_account = frappe.new_doc("Account")
+		customer_account.account_name = self.customer_name
+		customer_account.account_type = "Asset"
+		customer_account.account_balance = 0
+		customer_account.parent_account = customer_payments.account_name
+		customer_account.insert()
+		self.customer_account = customer_account.name
+		super().save(*args,**kwargs)
