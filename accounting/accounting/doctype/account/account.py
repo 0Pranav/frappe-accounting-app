88a29d3b2aea1b9a7b93ff20fcd7ee757b45cea2
pranav nachnekar <pranav@pranavs-MacBook-Pro.local>
2019-08-26 10:56:53 +0530

    Added document methods

--- accounting/accounting/doctype/account/account.py
+++ accounting/accounting/doctype/account/account.py
@@ -3,10 +3,42 @@
 # For license information, please see license.txt
 
 from __future__ import unicode_literals
-# import frappe
+import frappe
 from frappe.model.document import Document
 # import NestedSet
 from frappe.utils.nestedset import NestedSet
 
 class Account(NestedSet):
 	nsm_parent_field = "parent_account"
+
+	def debit(self, amount):
+		if self.account_type == "Asset" or self.account_type == "Expense":
+			# +
+			self.account_balance += amount
+			self.save()
+		else:
+			# -
+			self.account_balance -= amount
+			self.save()
+		if self.name == "Root":
+			return
+		if self.parent_account is not "Root":
+			parent=frappe.get_doc("Account",self.parent_account)
+			parent.debit(amount)
+		return	
+
+	def credit(self, amount):
+		if self.account_type == "Asset" or self.account_type == "Expense":
+			# -
+			self.account_balance -= amount
+			self.save()
+		else:
+			# +	
+			self.account_balance += amount
+			self.save()
+		if self.name == "Root":
+			return
+		if self.parent_account is not "Root":
+			parent=frappe.get_doc("Account",self.parent_account)
+			parent.credit(amount)
+		return	
