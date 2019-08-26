249a62a7dbda737beb2fb712af1e343bb452fa9f
pranav nachnekar <pranav@pranavs-MacBook-Pro.local>
2019-08-26 10:59:56 +0530

    Added payment entries

--- accounting/accounting/doctype/payment_entry/payment_entry.js
+++ accounting/accounting/doctype/payment_entry/payment_entry.js
@@ -2,16 +2,40 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Payment Entry', {
-	// refresh: function(frm) {
-
-	// }
-	refresh : function(frm){
-		frm.set_query("payment_type",()=>{
+	refresh: function (frm) {
+		if (frm.is_dirty()) {
+			console.log("h");
+			set_amount_to_remaining(frm)
+		}
+		frm.set_query("payment_type", () => {
 			return {
-				filters : {
-					name : ["in","Sales Invoice","Purchase Invoice"]
+				filters: {
+					name: ["in", ["Sales Invoice", "Purchase Invoice"]]
 				}
 			}
 		})
 	}
 });
+
+frappe.ui.form.on("Payment Entry", "against_invoice", set_amount_to_remaining)
+
+async function set_amount_to_remaining(frm) {
+	let invoice = await frappe.db.get_doc(frm.doc.payment_type, frm.doc.against_invoice)
+	let payment_entries = await frappe.db.get_list('Payment Entry', {
+		fields: ['name', 'payment_amount'],
+		filters: {
+			against_invoice: frm.doc.against_invoice,
+			docstatus: 1
+		}
+	})
+	let paid_amount;
+	paid_amount = payment_entries.reduce(get_paid_amount, 0)
+	let pending_amount = invoice.total - paid_amount;
+	frappe.model.set_value(frm.doctype, frm.docname, "payment_amount", pending_amount)
+	console.log([paid_amount, pending_amount, invoice.total]);
+	debugger
+}
+
+function get_paid_amount(total, entry) {
+	return total + entry.payment_amount
+}
--- accounting/accounting/doctype/payment_entry/payment_entry.py
+++ accounting/accounting/doctype/payment_entry/payment_entry.py
@@ -7,8 +7,25 @@ from __future__ import unicode_literals
 from frappe.model.document import Document
 
 class PaymentEntry(Document):
-	def save(self,*args,**args):
-		if self.invoice_type == "Sales Invoice":
-			print("Sales Invoice")
-		elif self.invoice_type == "Purchase Invoice":
-			print("Purchase Invoice")
+	def before_submit(self,*args,**kwargs):
+		invoice = frappe.get_doc(self.payment_type,self.against_invoice)
+		cash_account = frappe.get_doc("Account","Cash")
+
+		entries = frappe.get_list('Payment Entry',fields=['name','payment_amount'],filters=[["against_invoice","=",self.against_invoice],["docstatus","=",1]])
+		print(entries)
+		total = 0
+		for entry in entries:
+			total+= entry.payment_amount
+		pending_amount = invoice.total - total
+		print("pending amount:"+str(pending_amount))
+		if pending_amount < self.payment_amount:
+			frappe.throw("Don't overpay bruh")
+		if self.payment_type == "Sales Invoice":
+			credit_account = frappe.get_doc("Account",invoice.customer)
+			debit_account = cash_account
+		elif self.payment_type == "Purchase Invoice":
+			debit_account = frappe.get_doc("Account",invoice.supplier)
+			credit_account = cash_account
+		credit_account.credit(self.payment_amount)
+		debit_account.debit(self.payment_amount)	
+		GeneralLedger.make_double_entry(credit_account=credit_account,debit_account=debit_account,amount=self.payment_amount,txn_type="Payment Entry",txn_id=self.name)		
