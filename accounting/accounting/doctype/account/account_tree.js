baf068a179fd06b6afaa9a99db77579d1a96a78b
pranav nachnekar <pranav@pranavs-MacBook-Pro.local>
2019-08-26 10:56:19 +0530

    Added account balances

--- accounting/accounting/doctype/account/account_tree.js
+++ accounting/accounting/doctype/account/account_tree.js
@@ -1,2 +1,14 @@
 frappe.treeview_settings["Account"]={
+    breadcrumbs:"Account",
+    title:__("Chart of Accounts"),
+    root_label:"Accounts",
+    onrender:async function(node){
+        if(frappe.boot.user.can_read.indexOf("General Ledger") !== -1){ 
+            var res = await frappe.db.get_value("Account",node.title,['account_balance','account_type'])
+            debugger
+           if(node.data && res !== undefined){
+               $('<span class="balance-area pull-right text-muted small">'+res.message.account_balance+ (in_list(["Liability","Income"],res.message.account_type)?" Cr":" Dr")+'</span>').insertBefore(node.$ul)
+           } 
+        }
+    }
 }
