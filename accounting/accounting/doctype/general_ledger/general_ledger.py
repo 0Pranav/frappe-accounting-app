f15926d8e9197862492ab74288120af06822631e
pranav nachnekar <pranav@pranavs-MacBook-Pro.local>
2019-08-26 10:58:28 +0530

    Added static method for double entry

--- accounting/accounting/doctype/general_ledger/general_ledger.py
+++ accounting/accounting/doctype/general_ledger/general_ledger.py
@@ -3,8 +3,35 @@
 # For license information, please see license.txt
 
 from __future__ import unicode_literals
-# import frappe
+import frappe
+import datetime
 from frappe.model.document import Document
 
 class GeneralLedger(Document):
-	pass
+	@staticmethod
+	def make_double_entry(credit_account,debit_account,amount,txn_type,txn_id):
+		# Get today
+		today = datetime.date.today()
+		# Credit Entry
+		credit_entry = frappe.new_doc("General Ledger")
+		credit_entry.posting_date = today
+		credit_entry.account_affected = credit_account.name
+		credit_entry.credited_amount = amount
+		credit_entry.debited_amount = 0
+		credit_entry.account_balance = credit_account.account_balance
+		credit_entry.txn_type = txn_type
+		credit_entry.txn_id = txn_id
+		credit_entry.save()
+		# Debit Entry
+		debit_entry = frappe.new_doc("General Ledger")
+		debit_entry.posting_date = today
+		debit_entry.account_affected = debit_account.name
+		debit_entry.credited_amount = 0
+		debit_entry.debited_amount = amount
+		debit_entry.account_balance = debit_account.account_balance
+		debit_entry.txn_type = txn_type
+		debit_entry.txn_id = txn_id
+		debit_entry.save()
+
+		return
+
